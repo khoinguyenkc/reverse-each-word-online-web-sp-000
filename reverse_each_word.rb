aec097371638e503c3fb2e263c96fc870b33bc03
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-14 09:32:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array.collect do |word|
+  hi = array.collect do |word|
     word.reverse
   end
-  puts array
+  #how do i "save" what this collect method return
+  puts hi
 end
 
 reverse_each_word("ooh ooh baby")
