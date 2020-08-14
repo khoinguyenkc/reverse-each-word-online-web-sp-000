dcd416c7bb5f58466b85a9728d771bb4acc092d1
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-14 09:31:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
-def revrse_each_word(string)
+def reverse_each_word(string)
   array = string.split(" ")
   array.collect do |word|
     word.reverse
   end
   puts array
 end
+
+reverse_each_word("ooh ooh baby")
